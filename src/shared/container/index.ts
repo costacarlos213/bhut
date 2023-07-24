3c615c410a2e52db64bf8b1a2fbddeb4852bc4c5
Jimmy <carloscosta2912@gmail.com>
2023-07-23 21:15:45 -0300

    fix: add log singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,7 @@
+import { ILogRepository } from '@modules/Log/repositories/LogRepository.interface';
 import './providers';
+
+import { container } from 'tsyringe';
+import { LogRepository } from '@modules/Log/repositories/LogRepository';
+
+container.registerSingleton<ILogRepository>('LogRepository', LogRepository);
